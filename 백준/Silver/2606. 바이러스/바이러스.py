d4a62c3c588a228ddd3926e5da3b2172d386ee67
dragon4912 <dragon4912@naver.com>
2024-11-03 17:11:26 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -13,27 +13,18 @@ for _ in range(E):
     A[s].append(e)
     A[e].append(s)
 
-from collections import deque
-def BFS(v=1):
-    q = deque([v])
-    visited = [0] * (V+1)
-    visited[v] = 1
-
-    warm = 0
-    while q:
-        now = q.pop()
-        #1번 컴퓨터는 감염 컴퓨터 수에 카운트하지 않음
-
-        for next in A[now]:
-            if visited[next]:
-                continue
-            visited[next] = 1
-            q.append(next)
-            warm += 1       #연결이 있을떄마다 감염 컴퓨터 수 증가
-        
-        #print(q, visited)
+warm = 0
+visited = [0] * (V+1)
+def DFS(v):
+    global warm
     
-    return warm
+    for next in A[v]:
+        if visited[next]:
+            continue
+        visited[next] = True
+        DFS(next)
+        warm += 1
 
-warm_cnt = BFS(1)
-print(warm_cnt)
+visited[1] = True
+DFS(1)
+print(warm)
