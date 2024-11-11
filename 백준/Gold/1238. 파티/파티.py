0cd1db9a8c0cdb0fe007966433f6254b74a541fb
dragon4912 <dragon4912@naver.com>
2024-11-11 22:03:05 +0900

    [Gold III] Title: 파티, Time: 92 ms, Memory: 38948 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,15 +1,22 @@
 import sys
 input = sys.stdin.readline
 
+""" 두번쨰 풀이
+첫번째 풀이때는 각 노드->X까지의 최단거리를 찾기 위해 모든 노드에서 X까지 다익스트라를 구했다
+start->end를 뒤집은 인접리스트로 X로 시작해 다익스트라를 구하면 각 노드에서 X로 가는 최단거리가 된다
+"""
+
 N, M, X = map(int, input().split())     #마을수(=학생수), 도로수, 도착지
 
 A = [[] for _ in range(N+1)]    #0번 인덱스 미사용
+reverse_A  = [[] for _ in range(N+1)]
 for _ in range(M):
     s, e, w = map(int, input().split())
     A[s].append((e,w))
+    reverse_A[e].append((s,w))
 
 from queue import PriorityQueue
-def dijkstra(v):
+def dijkstra(A, v):
     D = [10**6] * (N+1)       #거리 최대값은 1000x100
     D[v] = 0        #시작마을은 0
     q = PriorityQueue()
@@ -29,11 +36,10 @@ def dijkstra(v):
                 q.put((D[next], next))
     return D
 
-answers = [0] * (N+1)
+forward_distance = dijkstra(reverse_A, X)       #각 노드에서 X까지 거리
+backward_distance = dijkstra(A, X)              #X에서 각 노드까지 거리
+
+answer = 0
 for v in range(1, N+1):
-    d = dijkstra(v)
-    answers[v] += d[X]
-    if v == X:
-        for e, dis in enumerate(d[1:], start=1):
-            answers[e] += dis
-print(max(answers))
+    answer = max(answer, forward_distance[v] + backward_distance[v])
+print(answer)
