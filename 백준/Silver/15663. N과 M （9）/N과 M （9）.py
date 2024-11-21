b2425d9f1a2b796ed1e1bf7627f62e6532eac44b
dragon4912 <dragon4912@naver.com>
2024-11-21 19:30:29 +0900

    [Silver II] Title: N과 M (9), Time: 92 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -5,22 +5,21 @@ N, M = map(int, input().split())
 A = list(map(int, input().split()))
 A.sort()
 
-from collections import deque
-def BFS():
-    q = deque([[]]) #빈 배열로 시작
-
-    while q:
-        elem_list = q.pop()
-        if len(elem_list) == M:
-            print(*[A[elem] for elem in elem_list])
+visited = [False] * N
+def DFS(elem_list):
+    if len(elem_list) == M:
+        print(*[A[elem] for elem in elem_list])
+        return
+    
+    last_insert_value = -1
+    for next_idx in range(0, N):
+        if A[next_idx] == last_insert_value:       #마지막으로 삽입했던 요소와 새로 삽입할 요소의 값이 같으면 패스
             continue
+        if visited[next_idx]:   #이미 있는 요소면 패스
+            continue
+        last_insert_value = A[next_idx]
+        visited[next_idx] = True
+        DFS(elem_list+[next_idx])
+        visited[next_idx] = False
 
-        last_insert_value = -1
-        for next_idx in range(0, N):
-            if A[next_idx] == last_insert_value:       #마지막으로 삽입했던 요소와 새로 삽입할 요소의 값이 같으면 패스
-                continue
-            if next_idx in elem_list:   #이미 있는 요소면 패스
-                continue
-            last_insert_value = A[next_idx]
-            q.appendleft(elem_list+[next_idx])
-BFS()
+DFS([])
