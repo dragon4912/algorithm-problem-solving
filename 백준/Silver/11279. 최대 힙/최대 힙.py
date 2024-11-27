0949ec60cbdf0d8b729bc105c9a34ce365b0711e
dragon4912 <dragon4912@naver.com>
2024-11-28 00:02:37 +0900

    [Silver II] Title: 최대 힙, Time: 112 ms, Memory: 37044 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -2,14 +2,14 @@ import sys
 input = sys.stdin.readline
 
 N = int(input().strip())
-from queue import PriorityQueue
-pq = PriorityQueue()
+import heapq
+heap = []
 for _ in range(N):
     cmd = int(input().strip())
     if cmd == 0:
-        if not pq.empty():
-            print(-pq.get())    #음수를 넣었으므로 양수로 바꿔 출력
+        if heap:
+            print(-heapq.heappop(heap))    #음수를 넣었으므로 양수로 바꿔 출력
         else:
             print(0)
     else:
-        pq.put(-cmd)    #큰값을 출력해야하므로 음수로 바꿔서 담음
+        heapq.heappush(heap, -cmd)    #큰값을 출력해야하므로 음수로 바꿔서 담음
