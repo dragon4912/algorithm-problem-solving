2292cdc7aad1759d4dd7bc2f3ffda4d7a172bbd3
dragon4912 <dragon4912@naver.com>
2024-12-01 13:57:45 +0900

    [Silver II] Title: 과일 탕후루, Time: 152 ms, Memory: 34600 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
@@ -5,32 +5,24 @@ N = int(input().strip())
 
 fruit_list = list(map(int, input().split()))
 
-fruit1 = None
-fruit2 = None
-max_cnt = 0
-fruit1_cnt = 0
-fruit2_cnt = 0
-fruit2_cnt_after_change = 0
-is_continuous = False
-for i in range(N):
-    if fruit_list[i] == fruit2:     #마지막 과일과 이전 과일이 같음
-        is_continuous = True
-        fruit2_cnt += 1
-        fruit2_cnt_after_change += 1
-    elif fruit_list[i] == fruit1:   #마지막 과일과 이전 과일이 다르지만 카운팅 되는 과일임
-        is_continuous = False
-        fruit1_cnt += 1
-    else:
-        is_continuous = False
+p_fruit_group_start = 0     #과일 그룹 시작 포인터
+p_fruit_group_end = 0       #과일 그룹 마지막 포인터
+second_fruit_idx = 0        #두 과일로 이루어진 그룹 중, 마지막 과일의 번호가 시작된 위치
+                            #ex) 현재 탐색 그룹이 1 2 2 1 1 1 이면, 마지막으로 1로만 이루어진 그룹이 시작한 위치인 4
+
+answer = 1
+for i in range(1, N):       #모두 0으로 초기화 했고, elif문의 idx-1 때문에 0은 제외
     
-    if not is_continuous:   #직전 과일과 현재 과일이 다른 경우
-        if fruit_list[i] == fruit1:
-            fruit1_cnt, fruit2_cnt = fruit2_cnt, fruit1_cnt
-        else:
-            fruit1_cnt, fruit2_cnt = fruit2_cnt_after_change, 1
-            fruit2_cnt_after_change = 1
-        fruit1 = fruit2
-        fruit2 = fruit_list[i]
+    if fruit_list[i] == fruit_list[second_fruit_idx]:       #마지막 과일과 현재 과일이 같으면
+        p_fruit_group_end = i
+    elif fruit_list[i] == fruit_list[second_fruit_idx-1]:      #두 과일중 마지막 과일이 아닌 과일이면
+        second_fruit_idx = i
+        p_fruit_group_end = i
+    else:   #새로운 과일이 나왔으면
+        p_fruit_group_start = second_fruit_idx
+        second_fruit_idx = i
+        p_fruit_group_end = i
     
-    max_cnt = max(max_cnt, fruit1_cnt+fruit2_cnt)
-print(max_cnt)
+    # print(f"{i} - 그룹 범위:{p_fruit_group_start}~{p_fruit_group_end} / 마지막 그룹 시작: {second_fruit_idx}")
+    answer = max(answer, p_fruit_group_end-p_fruit_group_start+1)
+print(answer)
